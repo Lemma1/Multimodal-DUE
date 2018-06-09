d51299b7161d80ee34052c6dc76771605cacbeaa
Lemma1 <lemma171@gmail.com>
2018-06-09 03:11:48 -0400

    add more modes and gradient projection methods

--- src/models.py
+++ src/models.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 from collections import OrderedDict
 
-IMPLEMENT_TYPES = ['single_drive', 'multiple_drive', 'transit', 'pnr']
+IMPLEMENT_TYPES = ['single_drive', 'multiple_drive', 'transit', 'pnr', 'metro']
 
 class parking_lot():
   def __init__(self, base_price):
@@ -25,13 +25,14 @@ class parking_lot():
 
 
 class base_path(object):
-  def __init__(self, target_time, early_penalty, late_penalty, ID_list, O, D):
+  def __init__(self, path_type, target_time, early_penalty, late_penalty, ID_list, O, D):
     self.target_time = target_time
     self.early_penalty = early_penalty
     self.late_penalty = late_penalty
     self.ID_list = ID_list
     self.O = O
     self.D = D
+    self.path_type = path_type
 
   def get_cost():
     raise Exception('Method in base class should not be called!')
@@ -43,14 +44,15 @@ class base_path(object):
     return np.max(self.late_penalty * (arrival_time - self.target_time), self.early_penalty * (self.target_time -arrival_time))
 
 class driving_route(base_path):
-  def __init__(self, target_time, early_penalty, late_penalty, O, D, ID_list, link_list, path_ID, number_people, parking_lot, walking_time):
+  def __init__(self, path_type, target_time, early_penalty, late_penalty, O, D, ID_list, link_list, path_ID, number_people, parking_lot, walking_time1, walking_time2):
     assert(number_people >= 1)
-    super(driving_route, self).__init__(target_time, early_penalty, late_penalty, ID_list, O, D)
+    super(driving_route, self).__init__(path_type, target_time, early_penalty, late_penalty, ID_list, O, D)
     self.link_list = link_list
     self.path_ID = path_ID
     self.number_people = number_people
     self.parking_lot = parking_lot
-    self.walking_time = walking_time
+    self.walking_time1 = walking_time1
+    self.walking_time2 = walking_time2
 
   def get_cost(self):
     return 0
@@ -62,36 +64,61 @@ class driving_route(base_path):
     return np.float(self.parking_fee) / np.float(number_people)
 
 class transit_route(base_path):
-  def __init__(self, target_time, early_penalty, late_penalty, O, D, ID_list, link_list, path_ID, transit_fare, walking_time, transit_waiting_time):
-    super(transit_route, self).__init__(target_time, early_penalty, late_penalty, ID_list, O, D)
+  def __init__(self, path_type, target_time, early_penalty, late_penalty, O, D, ID_list, link_list, path_ID, transit_fare, walking_time1, walking_time2, transit_time):
+    super(transit_route, self).__init__(path_type, target_time, early_penalty, late_penalty, ID_list, O, D)
     self.link_list = link_list
     self.path_ID = path_ID
     self.transit_fare = transit_fare
-    self.walking_time = walking_time
-    self.transit_waiting_time = transit_waiting_time
+    self.walking_time1 = walking_time1
+    self.walking_time2 = walking_time2
+    self.transit_time = transit_time
 
 class park_ride_route(base_path):
-  def __init__(self, target_time, early_penalty, late_penalty, O, D, ID_list, car_link_list, car_path_ID, 
+  def __init__(self, path_type, target_time, early_penalty, late_penalty, O, D, ID_list, car_link_list, car_path_ID, 
                         transit_link_list, transit_path_ID, parking_lot, transit_fare, 
-                        before_drive_walking_time, switching_time, after_transit_walking_time, transit_waiting_time):
-    super(park_ride_route, self).__init__(target_time, early_penalty, late_penalty, ID_list, O, D)
-    self.driving_part = driving_route(target_time, early_penalty, late_penalty, O, D, car_link_list, car_path_ID, 1, parking_lot, before_drive_walking_time)
-    self.transit_part = transit_route(target_time, early_penalty, late_penalty, O, D, transit_link_list, transit_path_ID, transit_fare, after_transit_walking_time, transit_waiting_time)
+                        before_drive_walking_time, switching_time, after_transit_walking_time, transit_time):
+    super(park_ride_route, self).__init__(path_type, target_time, early_penalty, late_penalty, ID_list, O, D)
+    self.driving_part = driving_route(path_type, target_time, early_penalty, late_penalty, O, D, ID_list, car_link_list, car_path_ID, 1, parking_lot, before_drive_walking_time, switching_time)
+    self.transit_part = transit_route(path_type, target_time, early_penalty, late_penalty, O, D, ID_list, transit_link_list, transit_path_ID, transit_fare, 0, after_transit_walking_time, transit_time)
 
+
+class metro(base_path):
+  def __init__(self, path_type, target_time, early_penalty, late_penalty, O, D, ID_list, walking_time1, metro_time, walking_time2, metro_fee):
+    super(metro, self).__init__(path_type, target_time, early_penalty, late_penalty, ID_list, O, D)
+    self.walking_time1 = walking_time1
+    self.walking_time2 = walking_time2
+    self.metro_time = metro_time
+    self.metro_fee = metro_fee
 
 def make_path(config):
   print "Creating route for:", config['mode']
   assert (config['mode'] in IMPLEMENT_TYPES)
   if config['mode'] == 'single_drive':
     assert (config['number_people'] == 1)
-    p = driving_route(config['target_time'], config['early_penalty'], config['late_penalty'], config['ID_list'],
-                      config['O'], config['D'],
-                      config['link_list'], config['path_ID'], 1, config['parking_lot'], config['walking_time'])
+    p = driving_route(config['mode'], config['target_time'], config['early_penalty'], config['late_penalty'],
+                      config['O'], config['D'], config['ID_list'],
+                      config['link_list'], config['path_ID'], 1, config['parking_lot'], config['walking_time1'], config['walking_time2'])
   if config['mode'] == 'multiple_drive':
     assert(config['number_people'] > 1)
-    p = driving_route(config['target_time'], config['early_penalty'], config['late_penalty'], config['ID_list'],
-                      config['O'], config['D'],
-                      config['link_list'], config['path_ID'], config['number_people'], config['parking_lot'], config['walking_time'])
+    p = driving_route(config['mode'], config['target_time'], config['early_penalty'], config['late_penalty'],
+                      config['O'], config['D'],  config['ID_list'],
+                      config['link_list'], config['path_ID'], config['number_people'], config['parking_lot'], config['walking_time1'], config['walking_time2'])
+  if config['mode'] == 'transit':
+    p = transit_route(config['mode'], config['target_time'], config['early_penalty'], config['late_penalty'],
+                      config['O'], config['D'], config['ID_list'],
+                      config['link_list'], config['path_ID'], config['transit_fare'], config['walking_time1'], 
+                      config['walking_time2'], config['transit_time'])
+  if config['mode'] == 'pnr':
+    p = park_ride_route(config['mode'], config['target_time'], config['early_penalty'], config['late_penalty'],
+                      config['O'], config['D'], config['ID_list'],  config['car_link_list'],
+                      config['car_path_ID'], config['transit_link_list'], config['transit_path_ID'], config['parking_lot'],
+                      config['transit_fare'], config['before_drive_walking_time'], config['switching_time'], config['after_transit_walking_time'],
+                      config['transit_time']
+                      )
+  if config['mode'] == 'metro':
+    p = metro(config['mode'], config['target_time'], config['early_penalty'], config['late_penalty'],
+                      config['O'], config['D'], config['ID_list'], config['walking_time1'], config['metro_time'], config['walking_time2'], config['metro_fee'])
+
   return p
 
 
@@ -107,22 +134,20 @@ class Multimode_DUE():
     car_flow = np.zeros((self.num_simulation_path, self.num_assign_interval))
     truck_flow = np.zeros((self.num_simulation_path, self.num_assign_interval))
     for i, path in enumerate(path_list):
-      if type(path) is driving_route:
+      # print type(path)
+      if path.path_type == 'single_drive' or path.path_type == 'multiple_drive':
         print "driving route"
         path_idx = self.simulation_path_list.index(path.path_ID)
+        # print path_idx
         car_flow[path_idx, :] = path_matrix[i, :] / np.float(path.number_people)
-      if type(path) is transit_route:
-        print "transit route"
-        # path_idx = self.simulation_path_list.index(path.path_ID)
-        # truck_flow[path_idx, :] = path_matrix[i, :]
-      if type(path) is park_ride_route:
+      if path.path_type == 'pnr':
         print "park and ride route"
         car_path_idx = self.simulation_path_list.index(path.driving_part.path_ID)
         car_flow[car_path_idx, :] = path_matrix[i, :]
         # path_idx = self.simulation_path_list.index(path.transit_part.path_ID)
         # truck_flow[path_idx, :] = path_matrix[i, :]
     truck_flow[2, :] = np.ones(self.num_assign_interval) * 10
-    truck_flow[7, :] = np.ones(self.num_assign_interval) * 10
+    # truck_flow[7, :] = np.ones(self.num_assign_interval) * 10
     return car_flow, truck_flow
 
 
